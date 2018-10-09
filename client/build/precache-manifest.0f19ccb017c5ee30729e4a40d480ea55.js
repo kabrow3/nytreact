395b80e87e2b20aaa7eb5bfbf1008c5ccf34408c
kabrow3 <kabrow3@gmail.com>
2018-10-08 21:50:25 -0400

    final

---
+++ client/build/precache-manifest.0f19ccb017c5ee30729e4a40d480ea55.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "8c42943e0c01c5602466",
+    "url": "/static/js/main.8c42943e.chunk.js"
+  },
+  {
+    "revision": "33bb9eb09db61bfb5ead",
+    "url": "/static/js/1.33bb9eb0.chunk.js"
+  },
+  {
+    "revision": "8c42943e0c01c5602466",
+    "url": "/static/css/main.90fc100d.chunk.css"
+  },
+  {
+    "revision": "aa9c97eaa461cf7de8d34a9538c8b763",
+    "url": "/index.html"
+  }
+];
